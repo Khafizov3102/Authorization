17829189436cd644620f69f863f81a1ad8fea3c3
Denis Khafizov <khafizov.den@yandex.ru>
2023-10-06 15:53:28 +0400

    Initial Commit

---
+++ Authorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authorization
+//
+//  Created by Денис Хафизов on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
