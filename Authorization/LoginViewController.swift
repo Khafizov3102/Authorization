37e646193ec83e5770d6c635f853565651b46afb
Denis Khafizov <khafizov.den@yandex.ru>
2023-10-06 20:09:04 +0400

    Homework 4

--- Authorization/LoginViewController.swift
+++ Authorization/LoginViewController.swift
@@ -7,17 +7,13 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    let username = "admin"
-    let password = "admin"
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    private let username = "admin"
+    private let password = "admin"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.identifier == "WelcomeSegue" else { return }
@@ -26,17 +22,81 @@ final class ViewController: UIViewController {
         destination.username = usernameTextField.text ?? ""
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard usernameTextField.text == username, passwordTextField.text == password else {
+            showLoginAlert()
+            usernameTextField.text = ""
+            passwordTextField.text = ""
+            return false
+        }
+        return true
+    }
+    
     @IBAction func forgotUsernameButtonPressed() {
+        showUsernameAlert()
     }
     
     @IBAction func forgotPassButtonPressed() {
+        showPasswordAlert()
     }
     
     @IBAction func loginButtonPressed() {
-        if usernameTextField.text == username &&
-            passwordTextField.text == password {
+        if shouldPerformSegue(withIdentifier: "WelcomeSegue", sender: self) {
             performSegue(withIdentifier: "WelcomeSegue", sender: self)
         }
+    }
+    
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    func showUsernameAlert() {
+        let usernameAlertController = UIAlertController(
+            title: "Forgot your username?",
+            message: "Your username is \(username)",
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        usernameAlertController.addAction(action)
+        
+        present(usernameAlertController, animated: true, completion: nil)
+    }
+    
+    func showPasswordAlert() {
+        let passwordAlertController = UIAlertController(
+            title: "Forgot password?",
+            message: "Your password is \(password)",
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        passwordAlertController.addAction(action)
+        
+        present(passwordAlertController, animated: true, completion: nil)
+    }
+    
+    func showLoginAlert() {
+        let loginAlertController = UIAlertController(
+            title: "Invalid username or password",
+            message: "Please, enter correct username and password",
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        loginAlertController.addAction(action)
+        
+        present(loginAlertController, animated: true, completion: nil)
     }
 }
 
